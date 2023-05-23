2a31baa9cfc9e5966c8e0c1cb9c012958b436e3c
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-05-23 23:03:33 +0700

    feat: add reactive and ref

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <script setup>
-// code
+import { ref, reactive } from 'vue';
+
+const count = ref(0);
+const counter = reactive({
+  count: 5
+});
+
+const increment = () => {
+  count.value++
+}
+const increment1 = () => {
+  counter.count++
+}
 </script>
 
 <template>
   <div>
-    <!-- HTML -->
+    <button @click="increment">
+      Count:
+      <strong>{{ count }}</strong>
+    </button>
+    <button @click="increment1">
+      Count1:
+      <strong>{{ counter.count }}</strong>
+    </button>
   </div>
 </template>
